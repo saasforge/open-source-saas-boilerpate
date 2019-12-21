3c0cb10c066423de10359d428b216eb4553481eb
saasforge <info@saasforge.dev>
2019-12-20 18:17:21 -0700

    Add payment information that can be used for storing payment or subscription data

--- src/shared/db_models/account.py
+++ src/shared/db_models/account.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import uuid
 from flask import current_app, request, url_for
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import UUID, JSONB
 
 from src.shared.utils.extensions import db
 from src.shared.db_models.account_history import AccountHistory
@@ -12,3 +12,7 @@ class Account(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=lambda: uuid.uuid4().hex)
     user = db.relationship('User', back_populates='account')
     account_history = db.relationship("AccountHistory", backref='account', lazy='dynamic')
+
+    # Payment information
+    payment_vendor_code = db.Column(db.String(64), nullable=True) # For example, 'stripe' or 'paypal'
+    payment_data = db.Column(JSONB(), nullable=True) # Vendor-specific information in Json format
