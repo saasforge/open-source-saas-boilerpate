91267086e340b4eefd59adb485c3720f71658275
saasforge <info@saasforge.dev>
2020-01-02 15:22:39 -0700

    Updated init scripts and installed redux

--- init.bat
+++ init.bat
@@ -1,3 +1,4 @@
+call python -m venv venv
 call venv\Scripts\activate.bat
 call python -m pip install --upgrade pip
 call pip install -r requirements.txt
--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+python -m venv venv
 source venv/bin/activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
