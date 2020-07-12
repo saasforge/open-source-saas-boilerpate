49a9a782c035c91a99741d431020a8ed2211891e
saasforge <info@saasforge.dev>
2020-07-12 15:39:43 -0600

    Fixed icon

--- src/components/icon/Icon.jsx
+++ src/components/icon/Icon.jsx
@@ -3,9 +3,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '@fortawesome/fontawesome-free-solid';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import './iconStyle.scss';
 
 library.add(fab);
+library.add(fas);
 
 class Icon extends Component {
     render() {
