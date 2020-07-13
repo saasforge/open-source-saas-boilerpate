ab9bb22eb7069d9f1a83b2bc5d3c34e530ca18b9
saasforge <info@saasforge.dev>
2020-07-13 10:06:46 -0600

    Added an optional user login wrapper

--- src/shared/utils/user_auth_wrapper.py
+++ src/shared/utils/user_auth_wrapper.py
@@ -3,11 +3,17 @@ from flask import request, current_app
 from flask import jsonify
 
 # Remove or replace this line to switch another way to authenticate
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity, jwt_optional
 
 def login_required(func):
     # Replace this line
     return jwt_required(func)
+
+# Use this wrapper if you don't require user to be authenticated.
+# To check if user is logged in, call get_current_user_id - it will return user's id
+# if a user is logged in else None
+def login_optional(func):
+    return jwt_optional(func)
 
 # Replace this line for any other authentication provider
 '''
